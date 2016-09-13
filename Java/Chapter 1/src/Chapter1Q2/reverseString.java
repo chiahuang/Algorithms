4a466268d5a3adbe9eecf2301fcf19219ec0a101
Chia Huang <chialh@uci.edu>
2016-09-12 19:28:31 -0700

    Add reverseString in java

--- Java/Chapter 1/src/Chapter1Q2/reverseString.java
+++ Java/Chapter 1/src/Chapter1Q2/reverseString.java
@@ -10,5 +10,37 @@ package Chapter1Q2;
  * @author Chia
  */
 public class reverseString {
-    
+
+    public reverseString() {
+
+    }
+
+    public static String reverse(String word, boolean recursion) {
+        if (word == null) {
+            return null;
+        }
+        if (word.equals("")) {
+            return "";
+        }
+        if (recursion) {
+            if (word.equals("")) {
+                return "";
+            }
+            else {
+                return word.charAt(word.length()-1) + reverse(word.substring(0, word.length()-1), true);
+            }
+
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int character = word.length() - 1; character >= 0; character--) {
+                sb.append(word.charAt(character));
+            }
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("pokemon", false));
+        System.out.println(reverse("creator", true));
+    }
 }
