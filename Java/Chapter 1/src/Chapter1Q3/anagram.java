c803e244f7a730d34aeb9f9387944f71f14290f1
Chia Huang <chialh@uci.edu>
2016-09-12 21:55:06 -0700

    Add permutation for java

--- Java/Chapter 1/src/Chapter1Q3/anagram.java
+++ Java/Chapter 1/src/Chapter1Q3/anagram.java
@@ -5,10 +5,54 @@
  */
 package Chapter1Q3;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  *
  * @author Chia
  */
 public class anagram {
+    public anagram() {
+        
+    }
     
+    public static boolean isPermutation(String word, String other) {
+        if(word == null || other == null) {
+            return false;
+        }
+        if(word.length() != other.length()) {
+            return false;
+        }
+        
+        HashMap hm = new HashMap<String, Integer>();
+        for(int character = 0; character < word.length(); character++) {
+            if(hm.containsKey(word.charAt(character))) {
+                int value = (int) hm.get(word.charAt(character)) + 1;
+                hm.put(word.charAt(character), value);
+            }
+            else {
+                hm.put(word.charAt(character), 1);
+            }
+        }
+        for(int letter = 0; letter <  other.length(); letter++) {
+            if(hm.containsKey(other.charAt(letter))) {
+                if ((int) hm.get(other.charAt(letter)) == 1) {
+                    hm.remove(other.charAt(letter));
+                }
+                else {
+                    int value = (int) hm.get(other.charAt(letter)) - 1;
+                    hm.put(other.charAt(letter), value);
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(isPermutation("dog god", "god dog"));
+    }
 }
